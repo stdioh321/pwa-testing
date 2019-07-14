60c1114159a672e56c16cacd9332f7f3583c821c
stdioh321 <diaslinoh@gmail.com>
2019-07-14 08:38:49 -0300

    Fix no login.

--- sw.js
+++ sw.js
@@ -1,58 +1,57 @@
-const PRECACHE = 'cache-v1.1';
-const RUNTIME = 'runtime';
-
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
+CACHE_NAME = "cache_v1.4";
+URL_CACHE = [
   'index.html',
-  './', // Alias for index.html
+  '/',
   'styles.css',
   "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
   "https://juststickers.in/wp-content/uploads/2014/05/Tux-Shape-Cut.png"
 ];
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
+  // console.log("installed");
+
+
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-
-self.addEventListener('fetch', event => {
-  // if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request)
-      .then(cachedResponse => {
-        console.log("Cache Match: ", cachedResponse)
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        cache.addAll(URL_CACHE);
       })
-    );
-  // }
+  );
+
+});
+
+self.addEventListener('activate', (event) => {
+
+  event.waitUntil(
+    caches.keys()
+      .then((cache) => {
+        Promise.all(
+          cache.filter(item => {
+            if (item != CACHE_NAME)
+              return caches.delete(item);
+          })
+        );
+      })
+  );
+  event.waitUntil(self.clients.claim());
+});
+
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(res => {
+        if (res) {
+          console.log("I'm on the cache", res);
+          return res;
+        }
+        return fetch(event.request, { "cache": "no-store" })
+          .then(res => {
+            console.log("Not cache", res);
+            return res;
+          }, err => {
+            return err;
+          })
+      })
+  );
 });
